4c1e910fcdc9cb3d7a0b918ca48bd2e1c824dd47
Shunsuke-mj <183798932+Shunsuke-mj@users.noreply.github.com>
2025-07-07 15:28:58 +0900

    Add files via upload
    Add common.html, index.html, common.css, index.css, hamberger.jpg, header-logo.jpg, gakki.jpg, script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+    var $button = this.document.querySelector('.toggle-menu-button');
+    var $menu = this.document.querySelector('.header-site-menu');
+    $button.addEventListener('click', function () {
+        if ($menu.classList.contains('is-show')){
+            $menu.classList.remove('is-show');
+        }
+        else {
+            $menu.classList.add('is-show');
+        }
+    });
+});
